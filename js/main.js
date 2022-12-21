772ae2842e1ade4099e0ca3170b4ac60d61ec915
KIM_SO_JEONG <ylnoevol@naver.com>
2022-12-21 15:57:59 +0900

    첫번째 섹션 scroll-spy 완성
    1. 두번째 섹션 리버스로 완성해야함.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
+
+/**
+ * 요소가 화면에 보여짐 여부에 따른 요소 관리
+ */
+// 관리할 요소들 검색!
 const spyEls = document.querySelectorAll('section.scroll-spy');
 // 요소들 반복 처리!
 spyEls.forEach(function (spyEl) {
   new ScrollMagic
     .Scene({ // 감시할 장면(Scene)을 추가
       triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
-      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+      triggerHook: 1.2 // 화면의 80% 지점에서 보여짐 여부 감시
     })
     .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
     .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
